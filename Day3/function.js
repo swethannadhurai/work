ec3d3e9d16d77bb7297222df7416ffe65769dadd
Swetha <swethaannadhurai@gmail.com>
2025-02-17 12:11:45 +0530

    landingpage2

--- Day3/function.js
+++ Day3/function.js
@@ -1,6 +1,10 @@
-function functionname(name){
-    console.log(name);
+var fruits = ["banana", "apple", "orange", "kiwi", "berry"];
+for(let i =0 ; i<fruits.length;i++){
+    console.log(fruits[i]);
 }
-functionname("Swetha");
-functionname("Rajeswari");
-functionname("Annadhurai");
+
+var fruits = ["banana", "apple", "orange", "kiwi", "berry"];
+
+ 
+let juicebasket = fruits.map((fruit)=> fruit+ " juice!");
+console.log(juicebasket);
